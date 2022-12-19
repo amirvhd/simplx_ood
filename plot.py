0b55432769489782551895e572141f2aa415550d
amirvhd <65691404+amirvhd@users.noreply.github.com>
2022-12-19 21:56:27 +0100

    adjust paths

---
+++ plot.py
@@ -0,0 +1,27 @@
+import numpy
+import pickle as pkl
+import matplotlib.pyplot as plt
+
+
+def manin():
+    with open('experiments/results/mnist/outlier/simplex_cifar_cv0.pkl', 'rb') as f:
+        data_base = pkl.load(f)
+    with open('experiments/results/mnist/outlier_sn/simplex_cifar_cv0.pkl', 'rb') as f:
+        data_sn = pkl.load(f)
+    pred_base = data_base.latent_approx()
+    error_base = ((data_base.test_latent_reps - pred_base) ** 2).sum(1).numpy()
+    pred_sn = data_sn.latent_approx()
+    error_sn = ((data_sn.test_latent_reps - pred_sn) ** 2).sum(1).numpy()
+    sorted_error_base = numpy.argsort(error_base)
+    sorted_error_base[sorted_error_base <= 10000] = 0
+    sorted_error_base[sorted_error_base > 10000] = 1
+    sorted_error_sn = numpy.argsort(error_sn)
+    sorted_error_sn[sorted_error_sn <= 10000] = 0
+    sorted_error_sn[sorted_error_sn > 10000] = 1
+    print(sum(sorted_error[:10000]))
+    print(sum(sorted_error_sn[:10000]))
+    # fig = plt.figure()
+    # plt.imshow(data, cmap="gray", interpolation="none")
+    # plt.title("CIFAR100-ood-detection")
+    # plt.xticks([])
+    # plt.yticks([])
