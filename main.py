80a5b0c3fdd7e48faaff52a7f8541f3d1f241c59
amirvhd <65691404+amirvhd@users.noreply.github.com>
2022-12-20 20:59:36 +0100

    adjust paths

--- main.py
+++ main.py
@@ -696,8 +696,8 @@ def outlier_detection(
         corpus_examples=corpus_features[:1000], corpus_latent_reps=corpus_latent_reps1[:1000]
     )
     simplex1.fit(
-        test_examples=test_features[:10000],
-        test_latent_reps=test_latent_reps1[:10000],
+        test_examples=test_features[10000:110000],
+        test_latent_reps=test_latent_reps1[10000:110000],
         n_epoch=n_epoch_simplex,
         reg_factor=0,
         n_keep=corpus_features.shape[0],
@@ -710,8 +710,8 @@ def outlier_detection(
         corpus_examples=corpus_features[:1000], corpus_latent_reps=corpus_latent_reps2[:1000]
     )
     simplex2.fit(
-        test_examples=test_features[:10000],
-        test_latent_reps=test_latent_reps2[:10000],
+        test_examples=test_features[10000:110000],
+        test_latent_reps=test_latent_reps2[10000:110000],
         n_epoch=n_epoch_simplex,
         reg_factor=0,
         n_keep=corpus_features.shape[0],
