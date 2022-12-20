2951b68411a935750e3b6098ec6500a9f32f5a5f
amirvhd <65691404+amirvhd@users.noreply.github.com>
2022-12-20 23:43:00 +0100

    adjust paths

--- plot.py
+++ plot.py
@@ -2,6 +2,7 @@ import numpy
 import pickle as pkl
 import matplotlib.pyplot as plt
 
+
 def main():
     with open('./experiments/results/cifar/outlier/simplex_cv0.pkl', 'rb') as f:
         data_base = pkl.load(f)
@@ -25,14 +26,22 @@ def main():
     # print(sum(sorted_error_sn[:10000]))
     cumulative = numpy.cumsum(sorted_error_base)
     cumulative2 = numpy.cumsum(sorted_error_sn)
+    x = numpy.randint(2, 20000)
+    cumulative3 = numpy.cumsum(x)
+    x_best = numpy.zeros(20000)
+    x_best[:10000]=1
+    cumulative4 = numpy.cumsum(x_best)
     plt.plot(cumulative, c='blue')
     plt.plot(cumulative2, c='green')
+    plt.plot(cumulative3, c='grey')
+    plt.plot(cumulative4, c='brown')
     plt.savefig('cifar100_cifar10.png')
     # fig = plt.figure()
     # plt.imshow(data, cmap="gray", interpolation="none")
     # plt.title("CIFAR100-ood-detection")
     # plt.xticks([])
     # plt.yticks([])
-if __name__ == "__main__":
 
+
+if __name__ == "__main__":
     main()
