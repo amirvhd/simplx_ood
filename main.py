2fa87446d5658b65223e7860f1083411b814545a
amirvhd <65691404+amirvhd@users.noreply.github.com>
2022-12-20 19:24:06 +0100

    adjust paths

--- main.py
+++ main.py
@@ -612,10 +612,10 @@ def outlier_detection(
     classifier1.load_state_dict(state_dict)
     classifier1.to(device)
     classifier1.eval()
-    save_path2 = "/dss/dssmcmlfs01/pn69za/pn69za-dss-0002/ra49bid2/cifar/outlier_sn/"
+    save_path2 = current_path / "experiments/results/cifar/outlier_sn/"
     new_state_dict = {}
     state_dict = torch.load(
-        os.path.join("/dss/dssmcmlfs01/pn69za/pn69za-dss-0002/ra49bid2/saved_models/BERD/", "best-checkpoint-v6.ckpt"),
+        os.path.join("/dss/dssmcmlfs01/pn69za/pn69za-dss-0002/ra49bid2/saved_models/BERD/", "best-checkpoint-v1.ckpt"),
         map_location=torch.device('cpu'))[
         "state_dict"]
     for k, v in state_dict.items():
@@ -692,20 +692,20 @@ def outlier_detection(
     test_features = torch.cat([cifar10_test_features, svhn_test_features], dim=0)
 
     # Fit corpus:
-    simplex1 = Simplex(
-        corpus_examples=corpus_features, corpus_latent_reps=corpus_latent_reps1
-    )
-    simplex1.fit(
-        test_examples=test_features[:10000],
-        test_latent_reps=test_latent_reps1[:10000],
-        n_epoch=n_epoch_simplex,
-        reg_factor=0,
-        n_keep=corpus_features.shape[0],
-    )
-    explainer_path = save_path1 / f"simplex_svhn1_cv{cv}.pkl"
-    with open(explainer_path, "wb") as f:
-        print(f"Saving simplex decomposition in {explainer_path}.")
-        pkl.dump(simplex1, f)
+    # simplex1 = Simplex(
+    #     corpus_examples=corpus_features, corpus_latent_reps=corpus_latent_reps1
+    # )
+    # simplex1.fit(
+    #     test_examples=test_features[:10000],
+    #     test_latent_reps=test_latent_reps1[:10000],
+    #     n_epoch=n_epoch_simplex,
+    #     reg_factor=0,
+    #     n_keep=corpus_features.shape[0],
+    # )
+    # # explainer_path = save_path1 / f"simplex_svhn1_cv{cv}.pkl"
+    # with open(explainer_path, "wb") as f:
+    #     print(f"Saving simplex decomposition in {explainer_path}.")
+    #     pkl.dump(simplex1, f)
     simplex2 = Simplex(
         corpus_examples=corpus_features, corpus_latent_reps=corpus_latent_reps2
     )
@@ -716,7 +716,8 @@ def outlier_detection(
         reg_factor=0,
         n_keep=corpus_features.shape[0],
     )
-    explainer_path = save_path2 / f"simplex_svhn1_cv{cv}.pkl"
+    # explainer_path = save_path2 / f"simplex_svhn1_cv{cv}.pkl"
+    explainer_path = "/dss/dssmcmlfs01/pn69za/pn69za-dss-0002/ra49bid2/cifar/outlier_sn/"
     with open(explainer_path, "wb") as f:
         print(f"Saving simplex decomposition in {explainer_path}.")
         pkl.dump(simplex2, f)
