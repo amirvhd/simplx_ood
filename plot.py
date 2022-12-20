67d3233457b48bce06eeff67fe154234a2146833
amirvhd <65691404+amirvhd@users.noreply.github.com>
2022-12-20 14:52:53 +0100

    adjust paths

--- plot.py
+++ plot.py
@@ -8,9 +8,9 @@ def main():
     with open('./experiments/results/cifar/outlier_sn/simplex_cv0.pkl', 'rb') as f:
         data_sn = pkl.load(f)
     pred_base = data_base.latent_approx()
-    error_base = ((data_base.test_latent_reps - pred_base) ** 2).sum(1).detach().numpy()
+    error_base = ((data_base.test_latent_reps - pred_base) ** 2).sum(1).cpu().numpy()
     pred_sn = data_sn.latent_approx()
-    error_sn = ((data_sn.test_latent_reps - pred_sn) ** 2).sum(1).detach().numpy()
+    error_sn = ((data_sn.test_latent_reps - pred_sn) ** 2).sum(1).cpu().numpy()
     sorted_error_base = numpy.argsort(error_base)
     sorted_error_base[sorted_error_base <= 10000] = 0
     sorted_error_base[sorted_error_base > 10000] = 1
