bb9af4a41b13ae6d5d3350b00b968b8455719eb2
amirvhd <65691404+amirvhd@users.noreply.github.com>
2022-12-20 11:02:13 +0100

    adjust paths

--- main.py
+++ main.py
@@ -696,13 +696,13 @@ def outlier_detection(
         corpus_examples=corpus_features, corpus_latent_reps=corpus_latent_reps1
     )
     simplex1.fit(
-        test_examples=test_features,
-        test_latent_reps=test_latent_reps1,
+        test_examples=test_features[:10000],
+        test_latent_reps=test_latent_reps1[:10000],
         n_epoch=n_epoch_simplex,
         reg_factor=0,
         n_keep=corpus_features.shape[0],
     )
-    explainer_path = save_path1 / f"simplex_svhn_cv{cv}.pkl"
+    explainer_path = save_path1 / f"simplex_svhn1_cv{cv}.pkl"
     with open(explainer_path, "wb") as f:
         print(f"Saving simplex decomposition in {explainer_path}.")
         pkl.dump(simplex1, f)
@@ -710,13 +710,13 @@ def outlier_detection(
         corpus_examples=corpus_features, corpus_latent_reps=corpus_latent_reps2
     )
     simplex2.fit(
-        test_examples=test_features,
-        test_latent_reps=test_latent_reps2,
+        test_examples=test_features[:10000],
+        test_latent_reps=test_latent_reps2[:10000],
         n_epoch=n_epoch_simplex,
         reg_factor=0,
         n_keep=corpus_features.shape[0],
     )
-    explainer_path = save_path2 / f"simplex_svhn_cv{cv}.pkl"
+    explainer_path = save_path2 / f"simplex_svhn1_cv{cv}.pkl"
     with open(explainer_path, "wb") as f:
         print(f"Saving simplex decomposition in {explainer_path}.")
         pkl.dump(simplex2, f)
