1190b5447f0fa05ac435c2cf172ab445936a7e19
amirvhd <65691404+amirvhd@users.noreply.github.com>
2022-12-22 00:40:51 +0100

    adjust paths

--- plot_cifar100.py
+++ plot_cifar100.py
@@ -119,11 +119,11 @@ def main():
     f_prob = numpy.zeros(10000)
     f_prob2 = numpy.zeros(10000)
     for i in range(10000):
-        if pdf_cifar10_base < 0.2:
+        if pdf_cifar10_base[i] < 0.2:
             f_prob[i] = pdf_cifar10_base[i]
         else:
             f_prob[i] = prob[i]
-        if pdf_cifar10_base < 0.2:
+        if pdf_cifar10_base[i] < 0.2:
             f_prob2[i] = pdf_cifar100_base[i]
         else:
             f_prob2[i] = prob2[i]
